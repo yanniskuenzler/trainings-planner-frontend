487580480669b0f6223e57ba6d7ce2a99080f114
Yannis Künzler <y.kuenzler@icloud.com>
2022-06-02 14:04:55 +0200

    initial commit

---
+++ src/components/trainingCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TrainingCard extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TrainingCard;
